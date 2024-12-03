14680bfac69ca87518320fa07422ea82e51350d0
mfw-bala <kamandla.b@moneyforward.co.jp>
2024-12-03 23:49:05 +0900

    fix footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -2,9 +2,8 @@ function LoadFooter() {
   fetch("components/footer/footer.html")
     .then((res) => res.text())
     .then((data) => {
-      const footer = document.createElement("footer");
+      const footer = document.getElementById("footer");
       footer.innerHTML = data;
-      document.body.appendChild(footer);
     })
     .catch((error) => {
       console.error("Error loading footer:", error);
