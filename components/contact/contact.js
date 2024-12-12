e6ed17a432d783d4cecfe070253bdcc33c1a5781
mfw-bala <kamandla.b@moneyforward.co.jp>
2024-12-12 16:44:00 +0900

    fix contact js code

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -5,6 +5,89 @@ async function LoadContact() {
   contact.innerHTML = data;
 }
 
+function validEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function isEmpty(value) {
+  return value.trim().length === 0;
+}
+
+function sendEmail() {
+  document.getElementById("submitform").addEventListener("click", (e) => {
+    e.preventDefault();
+    const FirstName = document.getElementById("FirstName").value;
+    const LastName = document.getElementById("LastName").value;
+    const email = document.getElementById("email").value;
+    const contentType = document.getElementById("contentType").value;
+    const description = document.getElementById("description").value;
+    const sendbtn = document
+      .getElementById("submitform")
+      .getElementsByTagName("i")[0];
+
+    if (isEmpty(FirstName)) {
+      document.getElementById("FirstName").placeholder = "must not be empty";
+      return;
+    } else {
+      document.getElementById("FirstName").placeholder = "First Name";
+    }
+    if (isEmpty(LastName)) {
+      document.getElementById("LastName").placeholder = "must not be empty";
+      return;
+    } else {
+      document.getElementById("LastName").placeholder = "Last Name";
+    }
+    if (isEmpty(email)) {
+      document.getElementById("email").placeholder = "email must not be empty";
+      return;
+    } else {
+      document.getElementById("email").placeholder = "Email";
+    }
+    if (isEmpty(contentType)) {
+      document.getElementById("contentType").placeholder = "must not be empty";
+
+      return;
+    } else {
+      document.getElementById("contentType").placeholder = "What's it about?";
+    }
+    if (description.trim().length == 0) {
+      document.getElementById("description").placeholder = "must not be empty";
+      return;
+    } else {
+      document.getElementById("description").placeholder = "Message";
+    }
+
+    if (!validEmail(email)) {
+      document.getElementById("email").placeholder = "Enter valid email";
+      document.getElementById("email").value = "";
+      return;
+    } else {
+      document.getElementById("email").placeholder = "Email";
+    }
+
+    sendbtn.textContent = " Sending...";
+    Email.send({
+      Host: "smtp.gmail.com",
+      Username: "protfoliobala@gmail.com",
+      Password: "password",
+      To: "kamandlabalashekhar@gmail.com",
+      From: "protfoliobala@gmail.com",
+      Subject: `<h1>${FirstName} ${LastName}</h1> send you a message`,
+      Body: `Name : <h2>${FirstName} ${LastName}</h2> <br/> Email : <h2>${email}</h2><br/> about : <h2>${contentType}</h2> <br/> description : <h2>${description}</h2>`,
+    }).then((message) => {
+      sendbtn.textContent = " Send Message";
+      document.getElementById("formid").reset();
+      document.getElementById("move").style.display = "flex";
+      setTimeout(() => {
+        document.getElementById("move").style.display = "none";
+      }, 5000);
+    });
+  });
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
   await LoadContact();
+  sendEmail();
 });
